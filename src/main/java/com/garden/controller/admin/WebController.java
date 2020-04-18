4bfee21c072c5476d557721d012122f2385452f3
wangpengfei <2539256533@qq.com>
2020-04-18 15:49:00 +0800

    修改端口

---
+++ src/main/java/com/garden/controller/admin/WebController.java
@@ -0,0 +1,29 @@
+package com.garden.controller.admin;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags ="业务相关接口")
+@RestController
+public class WebController {
+
+
+    @ApiOperation(value="hhh")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", defaultValue = "李四"),
+            @ApiImplicitParam(name = "address", value = "用户地址", defaultValue = "深圳", required = true)
+    }
+    )
+    @RequestMapping(value="hh",method= RequestMethod.GET)
+    public String hh() {
+        return "hello";
+    }
+
+
+
+}
